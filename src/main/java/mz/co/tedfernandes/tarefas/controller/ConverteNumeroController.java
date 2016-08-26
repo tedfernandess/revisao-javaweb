156482a54f59d3198e599da5e64205e0a5bd9258
tedfernandess <ted.fernandess@gmail.com>
2016-08-26 09:05:51 +0200

    Adicionando um mapeamento para uma página que contem o resultado da operação de conversão

--- src/main/java/mz/co/tedfernandes/tarefas/controller/ConverteNumeroController.java
+++ src/main/java/mz/co/tedfernandes/tarefas/controller/ConverteNumeroController.java
@@ -1,7 +1,12 @@
 package mz.co.tedfernandes.tarefas.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import mz.co.tedfernandes.tarefas.model.ConverteNumero;
 
 @Controller
 public class ConverteNumeroController {
@@ -14,6 +19,15 @@ public class ConverteNumeroController {
 	@RequestMapping("/romanoToDecimal")
 	public String romanoToDecimal() {
 		return "romanoToDecimal";
+	}
+	
+	@RequestMapping("converter")
+	public String converter(HttpServletRequest request, Model model) {
+		ConverteNumero converteNumero = new ConverteNumero();
+		String numeroRomano = request.getParameter("numeroRomano");
+		int resultado = converteNumero.converterNumeroRomano(numeroRomano);
+		model.addAttribute("resultado", resultado);
+		return "resultado";
 	}
 	
 }
