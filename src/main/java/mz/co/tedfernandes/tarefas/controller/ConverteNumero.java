c2caf2bde0c36cebe4cb0c6320a29318ffe8774d
tedfernandess <ted.fernandess@gmail.com>
2016-08-26 08:36:15 +0200

    Adicionando o primeiro cenário de teste

---
+++ src/main/java/mz/co/tedfernandes/tarefas/controller/ConverteNumero.java
@@ -0,0 +1,10 @@
+package mz.co.tedfernandes.tarefas.controller;
+
+public class ConverteNumero {
+
+	public int converterNumeroRomano(String texto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/test/java/mz/co/tedfernandes/tarefas/controller/ConverterNumeroTest.java
+++ src/test/java/mz/co/tedfernandes/tarefas/controller/ConverterNumeroTest.java
@@ -1,5 +1,15 @@
 package mz.co.tedfernandes.tarefas.controller;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ConverterNumeroTest {
 
+	@Test
+	public void deveConverterNumeroRomanoComUmCarater(){
+		ConverteNumero converteNumero = new ConverteNumero();
+		String texto="M";
+		Assert.assertEquals(1000, converteNumero.converterNumeroRomano(texto));
+	}
+	
 }
