9b21461889e1e3d7b64e255b9937fa933368079c
tedfernandess <ted.fernandess@gmail.com>
2016-08-26 08:40:39 +0200

    Mudando a assinatura do cenário de teste para receber um char como parametro

--- src/test/java/mz/co/tedfernandes/tarefas/controller/ConverterNumeroTest.java
+++ src/test/java/mz/co/tedfernandes/tarefas/controller/ConverterNumeroTest.java
@@ -8,8 +8,8 @@ public class ConverterNumeroTest {
 	@Test
 	public void deveConverterNumeroRomanoComUmCarater(){
 		ConverteNumero converteNumero = new ConverteNumero();
-		String texto="M";
-		Assert.assertEquals(1000, converteNumero.converterNumeroRomano(texto));
+		char letra='M';
+		Assert.assertEquals(1000, converteNumero.converterNumeroRomano(letra));
 	}
 	
 }
