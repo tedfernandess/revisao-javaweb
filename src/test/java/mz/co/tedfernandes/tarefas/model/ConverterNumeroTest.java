d32c3795ed989c144989dbe6497901cdbfea3e27
tedfernandess <ted.fernandess@gmail.com>
2016-08-26 12:06:57 +0200

    Refatorando a classe de testes de unidade

--- src/test/java/mz/co/tedfernandes/tarefas/model/ConverterNumeroTest.java
+++ src/test/java/mz/co/tedfernandes/tarefas/model/ConverterNumeroTest.java
@@ -1,29 +1,52 @@
 package mz.co.tedfernandes.tarefas.model;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConverterNumeroTest {
 
+	private ConverteNumero converterNumero;
+	
+	@Before
+	public void inicializacao(){
+		this.converterNumero = new ConverteNumero();
+	}
+	
 	@Test
 	public void deveConverterNumeroRomanoComUmCaraterValido(){
-		ConverteNumero converteNumero = new ConverteNumero();
 		String letra="M";
-		Assert.assertEquals(1000, converteNumero.executaOperacao(letra));
+		Assert.assertEquals(1000, this.converterNumero.executaOperacao(letra));
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void naoDeveConverterNumeroRomanoComUmCaraterInvalido(){
-		ConverteNumero converteNumero = new ConverteNumero();
-		String texto = "AAA";
-		converteNumero.executaOperacao(texto);
+		String texto = "A";
+		this.converterNumero.executaOperacao(texto);
 	}
 	
 	@Test
 	public void testarConversaoDoNumeroRomanoDentroDosLimitesEstabelecidos(){
-		ConverteNumero converteNumero = new ConverteNumero();
+		String texto="MMCMXCIX";
+		Assert.assertEquals(2999, this.converterNumero.executaOperacao(texto));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void naoDeveConverterNumeroRomanoForaDosLimitesEstabelecidos(){
+		String texto="MMMMCMXCIX";
+		this.converterNumero.executaOperacao(texto);
+	}
+	
+	@Test
+	public void deveConverterMenorNumeroRomano(){
+		String texto="I";
+		Assert.assertEquals(1, this.converterNumero.executaOperacao(texto));
+	}
+	
+	@Test
+	public void deveConverterMaiorNumeroRomanoEstabelecido(){
 		String texto="MMMCMXCIX";
-		Assert.assertEquals(3999, converteNumero.executaOperacao(texto));
+		Assert.assertEquals(3999, this.converterNumero.executaOperacao(texto));
 	}
 	
 }
