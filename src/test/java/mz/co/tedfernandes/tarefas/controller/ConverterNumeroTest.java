2fd5634e2950a0ff0fc59de61d66fbb488e1fb3e
tedfernandess <ted.fernandess@gmail.com>
2016-08-26 08:45:39 +0200

    Criando um cenário de teste para retornar 0 caso o caratere do número romano seja inválido

--- src/test/java/mz/co/tedfernandes/tarefas/controller/ConverterNumeroTest.java
+++ src/test/java/mz/co/tedfernandes/tarefas/controller/ConverterNumeroTest.java
@@ -6,10 +6,17 @@ import org.junit.Test;
 public class ConverterNumeroTest {
 
 	@Test
-	public void deveConverterNumeroRomanoComUmCarater(){
+	public void deveConverterNumeroRomanoComUmCaraterValido(){
 		ConverteNumero converteNumero = new ConverteNumero();
 		char letra='M';
 		Assert.assertEquals(1000, converteNumero.converterNumeroRomano(letra));
+	}
+	
+	@Test
+	public void naoDeveConverterNumeroRomanoComUmCaraterInvalido(){
+		ConverteNumero converteNumero = new ConverteNumero();
+		char letra='A';
+		Assert.assertEquals(0, converteNumero.converterNumeroRomano(letra));
 	}
 	
 }
