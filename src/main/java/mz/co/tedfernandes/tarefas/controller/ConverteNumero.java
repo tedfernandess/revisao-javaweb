38385edd91d3f16fbcb7eb417b38a1c2757a0fca
tedfernandess <ted.fernandess@gmail.com>
2016-08-26 08:41:29 +0200

    Adicionando a lógica para converter um número romano com um caratere apenas, para decimal

--- src/main/java/mz/co/tedfernandes/tarefas/controller/ConverteNumero.java
+++ src/main/java/mz/co/tedfernandes/tarefas/controller/ConverteNumero.java
@@ -2,9 +2,27 @@ package mz.co.tedfernandes.tarefas.controller;
 
 public class ConverteNumero {
 
-	public int converterNumeroRomano(String texto) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int converterNumeroRomano(char letra) {
+		switch (letra) {
+		case 'I':
+			return 1;
+		case 'X':
+			return 10;
+		case 'C':
+			return 100;
+		case 'M':
+			return 1000;
+
+		case 'V':
+			return 5;
+		case 'L':
+			return 50;
+		case 'D':
+			return 500;
+
+		default:
+			return 0;
+		}
 	}
 
 }
