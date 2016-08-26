17355b4dacb1c985fc8efe97765d249067e0ab65
tedfernandess <ted.fernandess@gmail.com>
2016-08-26 09:01:06 +0200

    Modificando o nome do controller e adicionando um mapeamento para o formulário de conversão de romano para decimal

--- src/main/java/mz/co/tedfernandes/tarefas/controller/ConverteNumeroController.java
+++ src/main/java/mz/co/tedfernandes/tarefas/controller/ConverteNumeroController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Teste {
+public class ConverteNumeroController {
 	
 	@RequestMapping("/olaSpring")
 	public String testa(){
 		return "teste";
+	}
+	
+	@RequestMapping("/romanoToDecimal")
+	public String romanoToDecimal() {
+		return "romanoToDecimal";
 	}
 	
 }
